f5d1b2de4c9381f92a11c7c404b8a24e84a84c8f
Miesvanderlippe <miesvanderlippe@gmail.com>
2019-05-02 16:55:45 +0200

    Dirmgr is required before the PPA etc can be added

--- system_setup.sh
+++ system_setup.sh
@@ -1,4 +1,7 @@
 # Java installer PPA
+apt-get update
+apt-get install dirmngr
+
 echo "deb http://ppa.launchpad.net/linuxuprising/java/ubuntu bionic main" | tee /etc/apt/sources.list.d/linuxuprising-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
 apt-get update
