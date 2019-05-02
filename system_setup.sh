a461ed5c9ff1b762e843959b4b0b2c60ec3fedfa
Miesvanderlippe <miesvanderlippe@gmail.com>
2019-05-02 14:08:38 +0200

    Added package removal

--- system_setup.sh
+++ system_setup.sh
@@ -4,7 +4,9 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
 apt-get update
 
 # Packages required for workstation
+apt-get remove $(grep -vE "^\s*#" unwanted.packages.list  | tr "\n" " ")
 apt-get install $(grep -vE "^\s*#" packages.list  | tr "\n" " ")
+apt-get autoremove
 
 # Add ifto user to sudo 
 usermod -aG sudo ifto
